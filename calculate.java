7d5dc1c73a8289f5feca18f7fb2ee1f242538f53
CharlzKoh <charlz_koh@i.ua>
2017-04-05 10:30:38 +0200

    add cum and sqr in calculate class

--- calculate.java
+++ calculate.java
@@ -2,5 +2,11 @@ public class calculate {
 
 	public static void main(String[] arg) {
 		System.out.println("calculate...");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int summ = first + second;
+		double sqr = Math.pow(first,second);
+		System.out.println("Sum "+ summ);
+		System.out.println("sqr "+sqr);
 	}
 }
